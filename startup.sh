f8bf1cd83e9ffc4e17af1cc3301abbef8c37d53f
Aswin Narayanan <aswinn89@gmail.com>
2021-06-24 00:48:56 +0000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,6 +2,7 @@
 
 service ssh start &&   \
 /usr/local/tomcat/bin/startup.sh; \
-su user -c "guacd -L debug -f"
+su user -c "USER=user /opt/TurboVNC/bin/vncserver -localhost -verbose -nohttpd -depth 24 -geometry $RES -securitytypes NONE -name \"VNC\" :1" && \
+guacd -L debug -f
 #su user -c "USER=user vncserver -depth 24 -geometry $RES -name \"VNC\" :1" && \
 # su user -c "USER=user /opt/TurboVNC/bin/vncserver -localhost -verbose -nohttpd -depth 24 -geometry $RES -securitytypes NONE -name \"VNC\" :1" && \
