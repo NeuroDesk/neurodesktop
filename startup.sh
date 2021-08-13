de8d65b819bf5da6a6df01117d0a46c3177024e0
Aswin Narayanan <aswinn89@gmail.com>
2021-08-13 05:24:17 +0000

    In startup.sh, move services to functions. Reformat output messages

--- startup.sh
+++ startup.sh
@@ -1,122 +1,113 @@
 #!/bin/bash
+# Credits to https://github.com/cyverse/ubuntu-desktop-docker
 
-full () {
-  echo \
+# help (){
+# echo "USAGE:
+# docker run -it -p 8080:8080 neuromachine:<tag> <option>
+# OPTIONS:
+# -v, --vnc  add VNC connection to Guacamole
+# -r, --rdp  add RDP connection to Guacamole
+# -s, --ssh  add SSH connection to Guacamole
+# -h, --help      print out this help
+# For more information see: https://github.com/NeuroDesk/neuromachine"
+# }
+
+open_guacmole_conf () {
+echo \
 "<user-mapping>
-    <authorize username=\"user\" password=\"password\">
-        <connection name=\"SSH\">
-            <protocol>ssh</protocol>
-            <param name=\"hostname\">localhost</param>
-            <param name=\"username\">user</param>
-            <param name=\"password\">password</param>
-            <param name=\"port\">22</param>
-            <param name=\"enable-sftp\">true</param>
-            <param name=\"sftp-root-directory\">/home/user</param>
-        </connection>
-        <connection name=\"VNC TigerVNC\">
-            <protocol>vnc</protocol>
-            <param name=\"hostname\">localhost</param>
-            <param name=\"username\">user</param>
-            <param name=\"password\">password</param>
-            <param name=\"port\">5901</param>
-            <param name=\"enable-sftp\">true</param>
-            <param name=\"sftp-username\">user</param>
-            <param name=\"sftp-password\">password</param>
-            <param name=\"sftp-directory\">/home/user</param>
-            <param name=\"sftp-root-directory\">/home/user</param>
-            <param name=\"enable-audio\">true</param>
-            <param name=\"audio-servername\">127.0.0.1</param>
-        </connection>
-        <connection name=\"VNC TurboVNC\">
-            <protocol>vnc</protocol>
-            <param name=\"hostname\">localhost</param>
-            <param name=\"username\">user</param>
-            <param name=\"password\">password</param>
-            <param name=\"port\">5902</param>
-            <param name=\"enable-sftp\">true</param>
-            <param name=\"sftp-username\">user</param>
-            <param name=\"sftp-password\">password</param>
-            <param name=\"sftp-directory\">/home/user</param>
-            <param name=\"sftp-root-directory\">/home/user</param>
-            <param name=\"enable-audio\">true</param>
-            <param name=\"audio-servername\">127.0.0.1</param>
-        </connection>
-        <connection name=\"RDP Fixed-size\">
-            <protocol>rdp</protocol>
-            <param name=\"hostname\">localhost</param>
-            <param name=\"username\">user</param>
-            <param name=\"password\">password</param>
-            <param name=\"port\">3389</param>
-            <param name=\"security\">any</param>
-            <param name=\"ignore-cert\">true</param>
-        </connection>
-        <connection name=\"RDP Auto-size\">
-            <protocol>rdp</protocol>
-            <param name=\"hostname\">localhost</param>
-            <param name=\"username\">user</param>
-            <param name=\"password\">password</param>
-            <param name=\"port\">3389</param>
-            <param name=\"security\">any</param>
-            <param name=\"ignore-cert\">true</param>
-            <param name=\"resize-method\">reconnect</param>
-        </connection>
-    </authorize>
-</user-mapping>" > /etc/guacamole/user-mapping.xml
+<authorize username=\"user\" password=\"password\">"  > /etc/guacamole/user-mapping.xml
+}
+
+close_guacmole_conf () {
+echo \
+"</authorize>
+</user-mapping>" >> /etc/guacamole/user-mapping.xml
+}
+
+ssh () {
+echo "\
+==================================================================
+Starting SSH server"
+service ssh start
+echo \
+"<connection name=\"SSH\">
+<protocol>ssh</protocol>
+<param name=\"hostname\">localhost</param>
+<param name=\"username\">user</param>
+<param name=\"password\">password</param>
+<param name=\"port\">22</param>
+<param name=\"enable-sftp\">true</param>
+<param name=\"sftp-root-directory\">/home/user</param>
+</connection>" >> /etc/guacamole/user-mapping.xml
+}
+
+vnc () {
+echo "\
+==================================================================
+Starting VNC server"
+su user -c "USER=user vncserver -depth $DEPTH -geometry $RES -name \"VNC\" :1"
+echo \
+"<connection name=\"VNC\">
+<protocol>vnc</protocol>
+<param name=\"hostname\">localhost</param>
+<param name=\"username\">user</param>
+<param name=\"password\">password</param>
+<param name=\"port\">5901</param>
+<param name=\"enable-sftp\">true</param>
+<param name=\"sftp-username\">user</param>
+<param name=\"sftp-password\">password</param>
+<param name=\"sftp-directory\">/home/user</param>
+<param name=\"sftp-root-directory\">/home/user</param>
+<param name=\"enable-audio\">true</param>
+<param name=\"audio-servername\">127.0.0.1</param>
+</connection>" >> /etc/guacamole/user-mapping.xml
+}
+
+rdp () {
+echo "\
+==================================================================
+Starting RDP server"
+service xrdp start
+echo \
+"<connection name=\"RDP\">
+<protocol>rdp</protocol>
+<param name=\"hostname\">localhost</param>
+<param name=\"username\">user</param>
+<param name=\"password\">password</param>
+<param name=\"port\">3389</param>
+<param name=\"security\">any</param>
+<param name=\"ignore-cert\">true</param>
+<param name=\"resize-method\">reconnect</param>
+</connection>" >> /etc/guacamole/user-mapping.xml
 }
 
 default () {
-  echo \
-"<user-mapping>
-    <authorize username=\"user\" password=\"password\">
-        <connection name=\"SSH\">
-            <protocol>ssh</protocol>
-            <param name=\"hostname\">localhost</param>
-            <param name=\"username\">user</param>
-            <param name=\"password\">password</param>
-            <param name=\"port\">22</param>
-            <param name=\"enable-sftp\">true</param>
-            <param name=\"sftp-root-directory\">/home/user</param>
-        </connection>
-        <connection name=\"VNC\">
-            <protocol>vnc</protocol>
-            <param name=\"hostname\">localhost</param>
-            <param name=\"username\">user</param>
-            <param name=\"password\">password</param>
-            <param name=\"port\">5901</param>
-            <param name=\"enable-sftp\">true</param>
-            <param name=\"sftp-username\">user</param>
-            <param name=\"sftp-password\">password</param>
-            <param name=\"sftp-directory\">/home/user</param>
-            <param name=\"sftp-root-directory\">/home/user</param>
-            <param name=\"enable-audio\">true</param>
-            <param name=\"audio-servername\">127.0.0.1</param>
-        </connection>
-        <connection name=\"RDP\">
-            <protocol>rdp</protocol>
-            <param name=\"hostname\">localhost</param>
-            <param name=\"username\">user</param>
-            <param name=\"password\">password</param>
-            <param name=\"port\">3389</param>
-            <param name=\"security\">any</param>
-            <param name=\"ignore-cert\">true</param>
-            <param name=\"resize-method\">reconnect</param>
-        </connection>
-    </authorize>
-</user-mapping>" > /etc/guacamole/user-mapping.xml
+    ssh
+    vnc
+    rdp
 }
 
-# full
-default
-# RES="2560x1440"
+
+
 RES="1920x1080"
+DEPTH="24"
 
-service ssh start &&   \
-service xrdp start && \
-su user -c "USER=user vncserver -depth 24 -geometry $RES -name \"VNC\" :1" && \
-/usr/local/tomcat/bin/startup.sh; \
+open_guacmole_conf
+default
+close_guacmole_conf
+
+echo "Starting tomcat server"
+/usr/local/tomcat/bin/startup.sh
+
+
+echo "\
+==================================================================
+Starting Guacamole Daemon
+------------------------------------------------------------------
+  Use this link for direct NeuroDesk Desktop:
+  http://localhost:8080/#/?username=user&password=password
+  Once connected to the session, your user info is:
+      Username: \"user\"
+      Password: \"password\"
+------------------------------------------------------------------"
 su user -c "guacd -L debug -f"
-
-
-# TurboVNC
-# su user -c "USER=user /opt/TurboVNC/bin/vncserver -localhost -verbose -nohttpd -depth 24 -geometry $RES -securitytypes NONE -name \"VNC\" :2" && \
-#su user -c "USER=user vncserver -depth 24 -geometry $RES -name \"VNC\" :1" && \
