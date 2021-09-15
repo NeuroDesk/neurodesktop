025807a7b75e854dff3e6c3d22b103504ec38daf
Steffen Bollmann <stebo85@users.noreply.github.com>
2021-09-15 20:47:21 +1000

    typos in start scripts and added new stratum1 servers

--- build_and_run.bat
+++ build_and_run.bat
@@ -1,3 +1,4 @@
+docker stop neurodesktop
 docker rm neurodesktop
 docker build -t neurodesktop:latest .
-docker run --shm-size=1gb -it --privileged --name neurodesktop -v ~/neurodesktop:/neurodesktop -e USER=user -p 8080:8080 neurodesktop:latest
+docker run --shm-size=1gb -it --privileged --name neurodesktop -v C:/neurodesktop:/neurodesktop -e USER=user -p 8080:8080 neurodesktop:latest
--- build_and_run.sh
+++ build_and_run.sh
@@ -1,3 +1,3 @@
-bash stop.sh
+bash stop_and_clean.sh
 docker build -t neurodesktop:latest .
 docker run --shm-size=1gb -it --privileged --name neurodesktop -v ~/neurodesktop:/neurodesktop -e USER=user -e HOST_UID="$(id -u)" -e HOST_GID="$(id -g)" -p 8080:8080 neurodesktop:latest
