10686e80a932f2de6f8ff0346f27aa0cafa618a2
Steffen Bollmann <stebo85@users.noreply.github.com>
2021-09-18 20:48:52 +1000

    test cvmfs deployment

---
+++ .github/workflows/test_cvmfs.sh
@@ -0,0 +1,71 @@
+SERVERIP=$1
+# SERVERIP=203.101.231.144
+
+sudo apt-get install lsb-release
+wget https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest_all.deb
+sudo dpkg -i cvmfs-release-latest_all.deb
+sudo apt-get update
+sudo apt-get install cvmfs
+
+sudo mkdir -p /etc/cvmfs/keys/ardc.edu.au/
+
+
+echo "-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwUPEmxDp217SAtZxaBep
+Bi2TQcLoh5AJ//HSIz68ypjOGFjwExGlHb95Frhu1SpcH5OASbV+jJ60oEBLi3sD
+qA6rGYt9kVi90lWvEjQnhBkPb0uWcp1gNqQAUocybCzHvoiG3fUzAe259CrK09qR
+pX8sZhgK3eHlfx4ycyMiIQeg66AHlgVCJ2fKa6fl1vnh6adJEPULmn6vZnevvUke
+I6U1VcYTKm5dPMrOlY/fGimKlyWvivzVv1laa5TAR2Dt4CfdQncOz+rkXmWjLjkD
+87WMiTgtKybsmMLb2yCGSgLSArlSWhbMA0MaZSzAwE9PJKCCMvTANo5644zc8jBe
+NQIDAQAB
+-----END PUBLIC KEY-----" | sudo tee /etc/cvmfs/keys/ardc.edu.au/neurodesk.ardc.edu.au.pub
+
+
+echo "CVMFS_USE_GEOAPI=yes" | sudo tee /etc/cvmfs/config.d/neurodesk.ardc.edu.au.conf
+
+echo "CVMFS_SERVER_URL=\"http://${SERVERIP}/cvmfs/@fqrn@\"" | sudo tee -a /etc/cvmfs/config.d/neurodesk.ardc.edu.au.conf
+
+echo 'CVMFS_KEYS_DIR="/etc/cvmfs/keys/ardc.edu.au/"' | sudo tee -a /etc/cvmfs/config.d/neurodesk.ardc.edu.au.conf
+
+echo "CVMFS_HTTP_PROXY=DIRECT" | sudo tee  /etc/cvmfs/default.local
+echo "CVMFS_QUOTA_LIMIT=5000" | sudo tee -a  /etc/cvmfs/default.local
+
+sudo cvmfs_config setup
+# sudo cvmfs_config wsl2_start
+sudo cvmfs_config chksetup
+
+ls /cvmfs/neurodesk.ardc.edu.au
+
+# sudo cvmfs_config umount
+
+# cvmfs_config stat -v neurodesk.ardc.edu.au
+
+
+
+## Test if containers are on CVMFS:
+wget https://raw.githubusercontent.com/NeuroDesk/neurocommand/main/cvmfs/log.txt
+echo "debug logfile:"
+cat log.txt
+
+while IFS= read -r LINE
+do
+    echo "LINE: $LINE"
+    IMAGENAME_BUILDDATE="$(cut -d' ' -f1 <<< ${LINE})"
+    echo "IMAGENAME_BUILDDATE: $IMAGENAME_BUILDDATE"
+
+    IMAGENAME="$(cut -d'_' -f1,2 <<< ${IMAGENAME_BUILDDATE})"
+    BUILDDATE="$(cut -d'_' -f3 <<< ${IMAGENAME_BUILDDATE})"
+    echo "[DEBUG] IMAGENAME: $IMAGENAME"
+    echo "[DEBUG] BUILDDATE: $BUILDDATE"
+
+    DIRECTORY=/cvmfs/neurodesk.ardc.edu.au/containers/${IMAGENAME}_${BUILDDATE}/
+    if [[ -d "$DIRECTORY" ]]
+    then
+        echo "$DIRECTORY exists on CVMFS."
+    else
+        echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+        echo "[DEBUG] $DIRECTORY does not exist on CVMFS"
+        echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+        exit 2
+    fi
+done < log.txt
