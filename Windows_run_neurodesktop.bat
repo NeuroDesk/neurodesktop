e5daa38a6165855fc4e1c97aec6d7de97524bf5e
thomshaw92 <thomasbasilshaw@gmail.com>
2021-11-30 17:23:29 +1000

    ping the website changed back to curl almost running now!!

--- Windows_run_neurodesktop.bat
+++ Windows_run_neurodesktop.bat
@@ -6,7 +6,6 @@ if "Get-Process 'com.docker.proxy' &&  echo $? " == "False" (
 echo "---------------------------------------------------------------"
   explorer "www.hub.docker.com/editions/community/docker-ce-desktop-windows"
 ) else (
-::"I don't like to think of PowerShell as "CMD with the stupid parts removed". I like to think of it as "Bash without any of the useful bits".
 ECHO "Starting NeuroDesktop:"
 ECHO "Remove old versions of NeuroDesktop - if none are available the daemon will throw an error which can be safely ignored: "
 ECHO "The following container has been stopped:"
@@ -14,20 +13,26 @@ docker stop neurodesktop
 ECHO "The following container has been removed:"
 docker rm neurodesktop
 
-ECHO "Running NeuroDesktop, please wait..."
-##https://superuser.com/questions/768553/start-program-wait-until-it-outputs-certain-string-then-continue 
-docker run --shm-size=1gb -it --privileged --name neurodesktop -v C:/neurodesktop-storage:/neurodesktop-storage -p 8080:8080 -h neurodesktop-20211028 vnmd/neurodesktop:20211028 |
-(:a
-if find "Guacamole proxy daemon (guacd)" || goto :next
-else)
+ECHO Starting NeuroDesktop, please wait...
+start powershell -windowstyle hidden -noexit -Command "docker run --shm-size=1gb -it --privileged --name neurodesktop -v C:/neurodesktop-storage:/neurodesktop-storage -p 8080:8080 -h neurodesktop-20211028 vnmd/neurodesktop:20211028"
+
+::poll for the guac server using curl  curl http://localhost:8080  
+:: possible responses while booting are curl: (52) Empty reply from server 
+:: if running it will say <!DOCTYPE html> 
+:: if not started it will say curl: (7) Failed to connect to localhost port 8080: Connection refused  
+
+:a
+if curl http://localhost:8080 | find "<!DOCTYPE html>" || goto :next
+else goto :b
+
+:b
 echo "waiting for neurodesk to start"
 timeout /t 5 /nobreak
-goto:a
-:next
+goto :a
 
+:next
 echo "Docker started, opening session"
 explorer "http://localhost:8080/#/?username=user&password=password"
-
 
 set /p=NeuroDesktop is running - press ENTER key to shutdown and quit NeuroDesktop!
 ECHO "The following container has been stopped:"
@@ -38,5 +43,4 @@ docker rm neurodesktop
 echo "-------------------------"
 echo "Docker Build failed!"
 echo "-------------------------"
-)
 )
