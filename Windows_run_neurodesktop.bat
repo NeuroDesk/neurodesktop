6ec2bba3e7287b5cf6ace29a0684859269757a74
thomshaw92 <thomasbasilshaw@gmail.com>
2021-12-01 12:02:25 +1000

    Working! yay!

--- Windows_run_neurodesktop.bat
+++ Windows_run_neurodesktop.bat
@@ -1,25 +1,24 @@
-rem "Checking if Docker is installed"
+ECHO "Checking if Docker is installed" 
+@echo off
 
-if "Get-Process 'com.docker.proxy' &&  echo $? " == "False" (
- echo "--------------------------------------------------------------"
- ECHO "Please follow the instruction to download and install Docker:"
-echo "---------------------------------------------------------------"
-  explorer "www.hub.docker.com/editions/community/docker-ce-desktop-windows"
-) else (
+
+
+docker ps 
+if %ERRORLEVEL% GEQ 1 goto setupdocker 
+
 ECHO "Starting NeuroDesktop:"
-ECHO "Remove old versions of NeuroDesktop - if none are available the daemon will throw an error which can be safely ignored: "
+ECHO "Remove old versions of NeuroDesktop - if none are available the daemon will throw an error which can be safely ignored:"
 ECHO "The following container has been stopped:"
 docker stop neurodesktop
 ECHO "The following container has been removed:"
 docker rm neurodesktop
 
-ECHO Starting NeuroDesktop, please wait...
+ECHO "Starting NeuroDesktop, please wait..."
 start powershell -windowstyle hidden -noexit -Command "docker run --shm-size=1gb -it --privileged --name neurodesktop -v C:/neurodesktop-storage:/neurodesktop-storage -p 8080:8080 -h neurodesktop-20211028 vnmd/neurodesktop:20211028"
-
-::poll for the guac server using curl  curl http://localhost:8080  
-:: possible responses while booting are curl: (52) Empty reply from server 
-:: if running it will say <!DOCTYPE html> 
-:: if not started it will say curl: (7) Failed to connect to localhost port 8080: Connection refused  
+::poll for the guac server using curl  curl http://localhost:8080
+:: possible responses while booting are curl: (52) Empty reply from server
+:: if running it will say <!DOCTYPE html>
+:: if not started it will say curl: (7) Failed to connect to localhost port 8080: Connection refused
 
 :loop
 echo "waiting for Neurodesk"
@@ -35,8 +34,12 @@ ECHO "The following container has been stopped:"
 docker stop neurodesktop
 ECHO "The following container has been removed:"
 docker rm neurodesktop
-) || (
-echo "-------------------------"
-echo "Docker Build failed!"
-echo "-------------------------"
-)
+exit 0
+
+:setupdocker
+echo "--------------------------------------------------------------"
+ECHO "Please follow the instruction to download and install Docker:"
+echo "---------------------------------------------------------------"
+explorer "https://docs.docker.com/desktop/windows/install/"
+set /p=press enter to quit, then try again once Docker is installed and running
+exit 0
