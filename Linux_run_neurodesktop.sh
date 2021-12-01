c7f023fc470e8653803d5124cdc93fb4a42f67d7
thomshaw92 <thomasbasilshaw@gmail.com>
2021-12-01 16:59:08 +1000

    Linux run script almost working Need to make contingencies for all OS.

---
+++ Linux_run_neurodesktop.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+#Tom Shaw 20211201
+#Check for Docker, open explorer if not and open docker download page
+#start the container and wait for the website to be available. 
+
+#first install xdg-tools
+REQUIRED_PKG="xdg-utils"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+echo "Checking for $REQUIRED_PKG: $PKG_OK"
+if [ "" = "$PKG_OK" ]; then
+  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+  sudo apt --yes install $REQUIRED_PKG
+fi
+
+echo "Checking if Docker is installed" 
+if ! docker info > /dev/null 2>&1; then
+    echo "This script uses docker, and it isn't running - please start docker and try again!"
+    echo "--------------------------------------------------------------"
+    echo "Or if, you haven't installed docker:"
+    echo "Please follow the instruction to download and install Docker:"
+    echo "---------------------------------------------------------------"
+    xdg-open "https://docs.docker.com/engine/install/"
+    echo "press any key to quit, then try again once Docker is installed and runningPress any key to continue"
+    while [ true ] ; do
+        read -t 3 -n 1
+        if [ $? = 0 ] ; then
+            exit ;
+            else
+            echo "waiting for the keypress"
+        fi
+    done
+else 
+    echo "Starting NeuroDesktop:"
+    echo "Remove old versions of NeuroDesktop"
+    echo "If none are available the daemon will throw an error which can be safely ignored:"
+    echo "Checking if the following container is available:"
+    docker stop neurodesktop
+    echo "Checking if the following container is available:"
+    docker rm neurodesktop
+    echo "--------------------------------------------------------------"
+    echo "Starting NeuroDesktop, please wait..."
+    echo "--------------------------------------------------------------"
+    mkdir -p ~/neurodesktop-storage
+    sudo docker run \
+    --shm-size=1gb -it -d --privileged --name neurodesktop \
+    -v ~/neurodesktop-storage:/neurodesktop-storage \
+    -e HOST_UID="$(id -u)" -e HOST_GID="$(id -g)" \
+    -p 8080:8080 -h neurodesktop-20211028 \
+    vnmd/neurodesktop:20211028 
+
+    #poll for the guac server using curl  curl http://localhost:8080
+    # possible responses while booting are curl: (52) Empty reply from server
+    # if running it will say <!DOCTYPE html>
+    # if not started it will say curl: (7) Failed to connect to localhost port 8080: Connection refused
+
+until curl http://localhost:8080 >/dev/null 2>&1 
+do
+echo "--------------------------------------------------------------"
+echo "Waiting for Neurodesk, please wait and your browser will open shortly"
+echo "--------------------------------------------------------------"
+function countdown(){
+   date1=$((`date +%s` + $1)); 
+   while [ "$date1" -ge `date +%s` ]; do 
+     echo -ne "$(date -u --date @$(($date1 - `date +%s`)) +%H:%M:%S)\r";
+     sleep 0.1
+   done
+}
+echo "Checking every 5 seconds"
+countdown 5
+done
+
+echo "Docker started, opening session"
+xdg-open "http://localhost:8080/#/?username=user&password=password"
+echo "NeuroDesktop is running - press ANY key to shutdown and quit NeuroDesktop!"
+while [ true ] ; do
+    read -n 1
+    if [ $? = 0 ] ; then
+    echo "The following container has been stopped:"
+    docker stop neurodesktop
+    echo "The following container has been removed:"
+    docker rm neurodesktop
+    exit 0
+    else
+    echo "Waiting for the keypress"
+    fi
+done
+fi
+
+
+#apt-get install xdg-utils
+# Ubuntu
+#apt-get install xdg-utils
+# Alpine
+#apk add xdg-utils
+# Arch Linux
+#pacman -S xdg-utils
+# Kali Linux
+#apt-get install xdg-utils
+# CentOS
+#yum install xdg-utils
+# Fedora
+#dnf install xdg-utils
+# Raspbian
+#apt-get install xdg-utils
