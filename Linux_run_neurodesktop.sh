4a4a6d2e6928c9ba043c4e3a0eb23c692fe573dd
thomshaw92 <thomasbasilshaw@gmail.com>
2021-12-02 11:25:36 +1000

    all package managers working Need to install xdg-utils on all still

--- Linux_run_neurodesktop.sh
+++ Linux_run_neurodesktop.sh
@@ -1,45 +1,79 @@
 #!/bin/bash
 #Tom Shaw 20211201
 #Check for Docker, open explorer if not and open docker download page
-#start the container and wait for the website to be available. 
+#start the container and wait for the website to be available.
 #function:
-function countdown(){
-   date1=$((`date +%s` + $1)); 
-   while [ "$date1" -ge `date +%s` ]; do 
-     echo -ne "$(date -u --date @$(($date1 - `date +%s`)) +%H:%M:%S)\r";
-     sleep 0.1
-   done
+function countdown() {
+    date1=$(($(date +%s) + $1))
+    while [ "$date1" -ge $(date +%s) ]; do
+        echo -ne "$(date -u --date @$(($date1 - $(date +%s))) +%H:%M:%S)\r"
+        sleep 0.1
+    done
 }
 
+  YUM_CMD=$(which yum)
+  APT_GET_CMD=$(which apt-get)
+  APT_CMD=$(which apt)
+  APK_CMD=$(which apk)
+  DNF_CMD=$(which dnf)
+  PACMAN_CMD=$(which pacman)
+
+if [[ ! -z $YUM_CMD ]]; then
+    yum install $YUM_PACKAGE_NAME
+elif [[ ! -z $APT_GET_CMD ]]; then
+    apt-get $DEB_PACKAGE_NAME
+elif [[ ! -z $APT_CMD ]]; then
+elif [[ ! -z $APT_CMD ]]; then
+elif [[ ! -z $APT_CMD ]]; then
+elif [[ ! -z $APT_CMD ]]; then
+    $OTHER_CMD <proper arguments>
+ else
+    echo "error can't install package $PACKAGE"
+    exit 1;
+ fi
+
 #first install xdg-tools
-# Check Os 
+# Check Os
 #apt-get install xdg-utils
 # Ubuntu
 #apt-get install xdg-utils
 # Alpine
+
+#apk search pkgName  apk search -v -d 'xdg-utils'
 #apk add xdg-utils
+
+
 # Arch Linux
+#pacman -Qs xdg-utils
 #pacman -S xdg-utils
+
+
 # Kali Linux
 #apt-get install xdg-utils
+
+
 # CentOS
 #yum install xdg-utils
+#yum list installed | grep xdg-utils
+
 # Fedora
 #dnf install xdg-utils
+#dnf list installed "xdg-utils"
+
 # Raspbian
 #apt-get install xdg-utils
 # package the file into a .desktop file like so
-#https://www.maketecheasier.com/create-desktop-file-linux/ 
+#https://www.maketecheasier.com/create-desktop-file-linux/
 REQUIRED_PKG="xdg-utils"
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG | grep "install ok installed")
 echo "Checking for $REQUIRED_PKG: $PKG_OK"
 if [ "" = "$PKG_OK" ]; then
-  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
-  sudo apt --yes install $REQUIRED_PKG
+    echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+    sudo apt --yes install $REQUIRED_PKG
 fi
 
-echo "Checking if Docker is installed" 
-if ! docker info > /dev/null 2>&1; then
+echo "Checking if Docker is installed"
+if ! docker info >/dev/null 2>&1; then
     echo "This script uses docker, and it isn't running - please start docker and try again!"
     echo "--------------------------------------------------------------"
     echo "Or if, you haven't installed docker:"
@@ -47,15 +81,15 @@ if ! docker info > /dev/null 2>&1; then
     echo "---------------------------------------------------------------"
     xdg-open "https://docs.docker.com/engine/install/"
     echo "press any key to quit, then try again once Docker is installed and runningPress any key to continue"
-    while [ true ] ; do
+    while [ true ]; do
         read -t 3 -n 1
-        if [ $? = 0 ] ; then
-            exit ;
+        if [ $? = 0 ]; then
+            exit
         else
             echo "waiting for the keypress"
         fi
     done
-else 
+else
     echo "Starting NeuroDesktop:"
     echo "Remove old versions of NeuroDesktop"
     echo "If none are available the daemon will throw an error which can be safely ignored:"
@@ -71,18 +105,18 @@ else
     mkdir -p ~/neurodesktop-storage
     #note this is in disconnected mode
     sudo docker run \
-    --shm-size=1gb -it -d --privileged --name neurodesktop \
-    -v ~/neurodesktop-storage:/neurodesktop-storage \
-    -e HOST_UID="$(id -u)" -e HOST_GID="$(id -g)" \
-    -p 8080:8080 -h neurodesktop-20211028 \
-    vnmd/neurodesktop:20211028 
+        --shm-size=1gb -it -d --privileged --name neurodesktop \
+        -v ~/neurodesktop-storage:/neurodesktop-storage \
+        -e HOST_UID="$(id -u)" -e HOST_GID="$(id -g)" \
+        -p 8080:8080 -h neurodesktop-20211028 \
+        vnmd/neurodesktop:20211028
 
     #poll for the guac server using curl  curl http://localhost:8080
     # possible responses while booting are curl: (52) Empty reply from server
     # if running it will say <!DOCTYPE html>
     # if not started it will say curl: (7) Failed to connect to localhost port 8080: Connection refused
 
-    until curl http://localhost:8080 >/dev/null 2>&1 ; do
+    until curl http://localhost:8080 >/dev/null 2>&1; do
         echo "--------------------------------------------------------------"
         echo "Waiting for Neurodesk, please wait and your browser will open shortly"
         echo "--------------------------------------------------------------"
@@ -94,9 +128,9 @@ else
     echo "Docker started, opening session"
     xdg-open "http://localhost:8080/#/?username=user&password=password"
     echo "NeuroDesktop is running - press ANY key to shutdown and quit NeuroDesktop!"
-    while [ true ] ; do
+    while [ true ]; do
         read -n 1
-        if [ $? = 0 ] ; then
+        if [ $? = 0 ]; then
             echo "The following container has been stopped:"
             docker stop neurodesktop
             echo "The following container has been removed:"
