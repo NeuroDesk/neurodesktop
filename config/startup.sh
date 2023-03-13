6a7a574ca53a0b3a9d2a1941e92229f131ce65ef
aswinnarayanan <aswinn89@gmail.com>
2023-03-13 07:40:49 +0000

    Update startup.sh for jupyter

--- config/startup.sh
+++ config/startup.sh
@@ -1,46 +1,40 @@
 #!/bin/bash
-# Credits to https://github.com/cyverse/ubuntu-desktop-docker
 
-# help (){
-# echo "USAGE:
-# docker run -it -p 8080:8080 neurodesktop:<tag> <option>
-# OPTIONS:
-# -v, --vnc  add VNC connection to Guacamole
-# -r, --rdp  add RDP connection to Guacamole
-# -s, --ssh  add SSH connection to Guacamole
-# -h, --help      print out this help
-# For more information see: https://github.com/NeuroDesk/neurodesktop"
-# }
+# clean up old session files (they prevent the start of the next session):
+echo "starting cleanup before if"
+if [ -f "/home/jovyan/.ssh/id_rsa" ]
+then
+    echo "starting cleanup"
+    rm /home/jovyan/.ssh/id_rsa
+    rm /home/jovyan/.ssh/authorized_keys
+    rm /home/jovyan/.ssh/id_rsa.pub
+    rm /home/jovyan/.ssh/ssh_host_rsa_key
+    rm /home/jovyan/.ssh/ssh_host_rsa_key.pub 
+    rm /home/jovyan/.ssh/sshd.pid
+    rm /home/jovyan/.Xauthority
+    rm -rf /home/jovyan/.dbus/session-bus
+    rm -rf /home/jovyan/.vnc
+    cp -r /tmp/jovyan/.vnc /home/jovyan/
+fi
 
-args=""
+# update example directory
+if [ -d "/home/jovyan/example-notebooks" ]
+then
+    cd /home/jovyan/example-notebooks
+    git pull
+else
+    git clone https://github.com/NeuroDesk/example-notebooks /home/jovyan/example-notebooks
+fi
 
-# Arguments
-POSITIONAL=()
-while [[ $# -gt 0 ]]
-   do
-   key="$1"
-
-   case $key in
-      --vnc)
-      vnc=true
-      shift # past argument
-      ;;
-      --rdp)
-      rdp=true
-      shift # past argument
-      ;;
-      *)    # unknown option
-      POSITIONAL+=("$1") # save it in an array for later
-      shift # past argument
-      ;;
-   esac
-done
-set -- "${POSITIONAL[@]}" # restore positional parameters
+# cvmfs2 -o config=/cvmfs/neurodesk.ardc.edu.au.conf neurodesk.ardc.edu.au /cvmfs/neurodesk.ardc.edu.au
+# ssh-keygen -t rsa -f /home/jovyan/.ssh/id_rsa -b 4096 -m PEM -N '' <<< n
+# ssh-keygen -t rsa -f /home/jovyan/.ssh/ssh_host_rsa_key -N '' <<< n
+# cat /home/jovyan/.ssh/id_rsa.pub >> /home/jovyan/.ssh/authorized_keys
 
 open_guacmole_conf () {
 echo \
 "<user-mapping>
-<authorize username=\"user\" password=\"password\">"  > /etc/guacamole/user-mapping.xml
+<authorize username=\"jovyan\" password=\"password\">"  > /etc/guacamole/user-mapping.xml
 }
 
 close_guacmole_conf () {
@@ -49,42 +43,33 @@ echo \
 </user-mapping>" >> /etc/guacamole/user-mapping.xml
 }
 
-ssh () {
-echo "\
-==================================================================
-Starting SSH server"
-service ssh restart
-echo \
-"<connection name=\"Command Line (SSH)\">
-<protocol>ssh</protocol>
-<param name=\"hostname\">localhost</param>
-<param name=\"username\">user</param>
-<param name=\"password\">password</param>
-<param name=\"port\">22</param>
-<param name=\"enable-sftp\">true</param>
-<param name=\"sftp-root-directory\">/home/user/Desktop</param>
-</connection>" >> /etc/guacamole/user-mapping.xml
-}
-
 vnc () {
 echo "\
 ==================================================================
 Starting VNC server"
-su user -c "USER=user vncserver -kill :1"
-su user -c "USER=user vncserver -depth 24 -geometry 1920x1080 -name \"VNC\" -localhost :1"
+# su user -c "USER=user vncserver -kill :1"
+# su user -c "USER=user vncserver -depth 24 -geometry 1920x1080 -name \"VNC\" -localhost :1"
+USER=jovyan vncserver -kill :1
+USER=jovyan vncserver -depth 24 -geometry 1920x1080 -name \"VNC\" :1
+xset -display :1 s off
 echo \
 "<connection name=\"Desktop Fixed-Resolution (VNC)\">
 <protocol>vnc</protocol>
 <param name=\"hostname\">localhost</param>
-<param name=\"username\">user</param>
+<param name=\"username\">jovyan</param>
 <param name=\"password\">password</param>
 <param name=\"port\">5901</param>
 <param name=\"enable-sftp\">true</param>
-<param name=\"sftp-username\">user</param>
-<param name=\"sftp-password\">password</param>
-<param name=\"sftp-directory\">/home/user/Desktop</param>
-<param name=\"sftp-root-directory\">/home/user/Desktop</param>
-<param name=\"enable-audio\">true</param>
+<param name=\"sftp-port\">2222</param>
+<param name=\"sftp-username\">jovyan</param>" >> /etc/guacamole/user-mapping.xml
+
+echo "<param name=\"sftp-private-key\">$(cat /home/jovyan/.ssh/id_rsa)" >> /etc/guacamole/user-mapping.xml
+
+echo \
+"</param>
+<param name=\"sftp-directory\">/home/jovyan/Desktop</param>
+<param name=\"sftp-root-directory\">/home/jovyan/Desktop</param>
+<param name=\"enable-audio\">false</param>
 <param name=\"audio-servername\">127.0.0.1</param>
 </connection>" >> /etc/guacamole/user-mapping.xml
 }
@@ -93,25 +78,24 @@ rdp () {
 echo "\
 ==================================================================
 Starting RDP server"
-service xrdp restart
+sudo service xrdp restart
 echo \
 "<connection name=\"Desktop Auto-Resolution (RDP)\">
 <protocol>rdp</protocol>
 <param name=\"hostname\">localhost</param>
-<param name=\"username\">user</param>
+<param name=\"username\">jovyan</param>
 <param name=\"password\">password</param>
 <param name=\"port\">3389</param>
 <param name=\"security\">any</param>
 <param name=\"ignore-cert\">true</param>
 <param name=\"resize-method\">reconnect</param>
 <param name=\"enable-drive\">true</param>
-<param name=\"drive-path\">/home/user/Desktop</param>
+<param name=\"drive-path\">/home/jovyan/Desktop</param>
 </connection>" >> /etc/guacamole/user-mapping.xml
 }
 
 default=true
 open_guacmole_conf
-ssh
 if [ "$rdp" = true ]; then
     rdp
     default=""
@@ -121,72 +105,25 @@ if [ "$vnc" = true ]; then
     default=""
 fi
 if [ "$default" = true ]; then
+    # vnc
     rdp
-    vnc
 fi
 close_guacmole_conf
 
 export JAVA_OPTS="-Xms512M -Xmx1024M"
 export CATALINA_OPTS="-Xms512M -Xmx1024M"
 
-if [ -n "$HOST_UID" ]; then
-    echo "Setting UID to $HOST_UID"
-    usermod -u $HOST_UID user
-fi
-if [ -n "$HOST_GID" ]; then
-    echo "Setting GID to $HOST_GID"
-    groupmod -g $HOST_GID user
-    chgrp +$HOST_GID /home/user
-fi
-cd /home/user
+# cd /neurocommand
+# git fetch
+# git pull
+# bash build.sh
+# cd /home/jovyan
 
-# Create vscode config on persistant storage
-mkdir -p /neurodesktop-storage/.config/Code
-chown -R user:user /neurodesktop-storage/.config
-mkdir -p /neurodesktop-storage/.vscode
-chown -R user:user /neurodesktop-storage/.vscode
+# /usr/sbin/sshd -f /home/jovyan/.ssh/sshd_config
+# USER=jovyan vncserver -kill :1
+# USER=jovyan vncserver -depth 24 -geometry 1920x1080 -name \"VNC\" :1
+# xset -display :1 s off
 
-if [ -z "$CVMFS_DISABLE" ]; then
-    echo "\
-    ==================================================================
-    Mounting CVMFS"
-    sudo mkdir /cvmfs/neurodesk.ardc.edu.au
-    sudo mount -t cvmfs neurodesk.ardc.edu.au /cvmfs/neurodesk.ardc.edu.au
-
-    echo "\
-    ==================================================================
-    Testing which CVMFS server is fastest"
-    sudo cvmfs_talk -i neurodesk.ardc.edu.au host probe
-    sudo cvmfs_talk -i neurodesk.ardc.edu.au host info
-fi
-
-
-echo "\
-==================================================================
-Starting tomcat server"
 /usr/local/tomcat/bin/startup.sh
-
-echo "\
-==================================================================\
-
-Starting Neurodesktop
-------------------------------------------------------------------
-
-    Use this link for accessing Neurodesktop in your Browser:
-!!! http://localhost:8080/#/?username=user&password=password !!!
-    
-------------------------------------------------------------------
-
-    Once connected to the session, your user info is:
-    Username: \"user\"
-    Password: \"password\"
-
-    If you want to stop Neurodesktop:
-    1) Hit CTRL-C
-    2) Type: docker stop neurodesktop
-    3) Hit Enter
-    4) Type: docker rm neurodesktop
-    5) Hit Enter
-=================================================================="
-# su user -c "guacd -f -L debug && echo"
-su user -c "guacd -f -b 127.0.0.1 && echo"
+guacd -b 127.0.0.1
+echo "===========================================================" 
