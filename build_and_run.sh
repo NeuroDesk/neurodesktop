e2deeaa2890ca8aa6c9c0a092dc2dc3a247a1c15
aswinnarayanan <aswinn89@gmail.com>
2023-03-14 07:08:16 +0000

    Fix UID GID and chown for linux hosts

--- build_and_run.sh
+++ build_and_run.sh
@@ -7,11 +7,9 @@ fi
 # docker run --shm-size=1gb -it --privileged --name neurodesktop -v ~/neurodesktop-storage:/neurodesktop-storage -e HOST_UID="$(id -u)" -e HOST_GID="$(id -g)" -p 8080:8080 neurodesktop:latest
 # -e CVMFS_DISABLE=true # will disable CVMFS for testing purposes
 
-
 docker build . -t neurodesktop:latest
-# docker run --shm-size=1gb -it --privileged --name neurodesktop -p 8888:8888 vnmd/jupyter-remote-desktop-proxy:$timestamp
-# docker run --shm-size=1gb -it --cap-add SYS_ADMIN --security-opt apparmor:unconfined --device=/dev/fuse --name neurodesktop -v /cvmfs:/cvmfs -p 8888:8888 neurodesktop:latest
 
-docker run --shm-size=1gb -it --cap-add SYS_ADMIN --security-opt apparmor:unconfined --device=/dev/fuse --name neurodesktop -v /cvmfs:/cvmfs -p 8888:8888 -e NB_UID="$(id -u)" -e NB_GID="$(id -g)" -e GRANT_SUDO=yes neurodesktop:latest
-
-
+docker run --shm-size=1gb -it --cap-add SYS_ADMIN --security-opt apparmor:unconfined \
+    --device=/dev/fuse --name neurodesktop -v /cvmfs:/cvmfs -p 8888:8888 \
+    -e NB_UID="$(id -u)" -e NB_GID="$(id -g)" -e CHOWN_HOME=yes -e CHOWN_HOME_OPTS='-R' \
+    neurodesktop:latest
