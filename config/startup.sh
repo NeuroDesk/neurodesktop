801bde4b73dad8926077940446a6b31617ccdcc9
aswinnarayanan <aswinn89@gmail.com>
2023-03-21 04:24:31 +0000

    Reduce neurodesktop startup.sh to core neurodesktop services

--- config/startup.sh
+++ config/startup.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# if [ -z "$CVMFS_DISABLE" ]; then
-#     echo "\
-#     ==================================================================
-#     Mounting CVMFS"
-#     sudo mkdir /cvmfs/neurodesk.ardc.edu.au
-#     sudo mount -t cvmfs neurodesk.ardc.edu.au /cvmfs/neurodesk.ardc.edu.au
-
-#     echo "\
-#     ==================================================================
-#     Testing which CVMFS server is fastest"
-#     sudo cvmfs_talk -i neurodesk.ardc.edu.au host probe
-#     sudo cvmfs_talk -i neurodesk.ardc.edu.au host info
-# fi
-
 export JAVA_OPTS="-Xms512M -Xmx1024M"
 export CATALINA_OPTS="-Xms512M -Xmx1024M"
 sudo /usr/local/tomcat/bin/startup.sh
