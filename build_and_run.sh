a79cd3fa0d18bc353515ca9d545526369e8c3f9a
aswinnarayanan <aswinn89@gmail.com>
2023-03-23 08:31:48 +0000

    Mount cvmfs and neurodesktop-storage

--- build_and_run.sh
+++ build_and_run.sh
@@ -10,6 +10,7 @@ fi
 docker build . -t neurodesktop:latest
 
 docker run --shm-size=1gb -it --cap-add SYS_ADMIN --security-opt apparmor:unconfined \
-    --device=/dev/fuse --name neurodesktop -v /cvmfs:/cvmfs -p 8888:8888 \
-    --user=root -e NB_UID="$(id -u)" -e NB_GID="$(id -g)" \
+    --device=/dev/fuse --name neurodesktop -v ~/neurodesktop-storage:/neurodesktop-storage \
+    -v /cvmfs:/cvmfs -p 8888:8888 \
+    --user=root -e NB_UID="$(id -u)" -e NB_GID="$(id -g)" -e GRANT_SUDO=yes \
     neurodesktop:latest
