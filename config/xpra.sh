3b535bf62b7f10093e669235accda90b13a55256
aswinnarayanan <aswinn89@gmail.com>
2023-03-24 04:06:59 +0000

    Add xpra setup config

--- config/xpra.sh
+++ config/xpra.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-xpra start --html=on --bind-tcp=0.0.0.0:9090 --start=xterm --no-daemon
+xpra start \
+    --html=on \
+    --bind-tcp=0.0.0.0:9090 \
+    --start=xterm \
+    --no-daemon \
+    --clipboard-direction=both \
+    --keyboard-sync=no \
+    --no-mdns \
+    --no-bell \
+    --no-speaker \
+    --no-printing \
+    --no-microphone \
+    --no-notifications \
+    --no-systemd-run
