07f41cf740625b07c69abdf9fdeffd683f42cd5e
aswinnarayanan <aswinn89@gmail.com>
2023-05-23 03:46:23 +0000

    Remove buildx from build_and_run scripts

--- build_and_run.sh
+++ build_and_run.sh
@@ -7,7 +7,7 @@ fi
 # docker run --shm-size=1gb -it --privileged --name neurodesktop -v ~/neurodesktop-storage:/neurodesktop-storage -e HOST_UID="$(id -u)" -e HOST_GID="$(id -g)" -p 8080:8080 neurodesktop:latest
 # -e CVMFS_DISABLE=true # will disable CVMFS for testing purposes
 
-docker buildx build . -t neurodesktop:latest
+docker build . -t neurodesktop:latest
 
 # Test with internal CVMFS
 docker run --shm-size=1gb -it --cap-add SYS_ADMIN --security-opt apparmor:unconfined \
