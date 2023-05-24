72598d068e025b8518f6fecb9d25aea46157a9bf
aswinnarayanan <aswinn89@gmail.com>
2023-05-24 04:53:26 +0000

    Remove unused c.ServerProxy

--- config/jupyter/jupyter_notebook_config.py
+++ config/jupyter/jupyter_notebook_config.py
@@ -12,20 +12,7 @@ c.ServerProxy.servers = {
         'title': 'Neurodesktop',
         'icon_path': '/opt/neurodesk_brain_logo.svg'
       }
-    },
-  # 'xneurodesk': {
-  #   'command': ['/opt/neurodesktop/xpra.sh'],
-  #   'port': 9090,
-  #   'timeout': 60,
-  #     'request_headers_override': {
-  #         'Authorization': 'Basic am92eWFuOnBhc3N3b3Jk',
-  #     },
-  #     'launcher_entry': {
-  #       'path_info' : 'xneurodesk',
-  #       'title': 'XNeurodesk',
-  #       'icon_path': '/opt/neurodesk_brain_icon.svg'
-  #     }
-  #   }
+    }
 }
 c.ServerApp.root_dir = '/'
 c.ServerApp.preferred_dir = os.getcwd()
