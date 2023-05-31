294a32d63dcb9f763168bb3dcc5fe98a08767a59
Steffen Bollmann <s.bollmann@uq.edu.au>
2023-05-31 05:25:52 +0000

    test with internal cvmfs

--- build_and_run.sh
+++ build_and_run.sh
@@ -10,15 +10,15 @@ fi
 docker build . -t neurodesktop:latest
 
 # Test with internal CVMFS
-# docker run --shm-size=1gb -it --cap-add SYS_ADMIN --security-opt apparmor:unconfined \
-#     --device=/dev/fuse --name neurodesktop -v ~/neurodesktop-storage:/neurodesktop-storage \
-#     -p 8888:8888 \
-#     --user=root -e NB_UID="$(id -u)" -e NB_GID="$(id -g)" \
-#     neurodesktop:latest
-
-# Run with external CVMFS:
 docker run --shm-size=1gb -it --cap-add SYS_ADMIN --security-opt apparmor:unconfined \
     --device=/dev/fuse --name neurodesktop -v ~/neurodesktop-storage:/neurodesktop-storage \
-    -v /cvmfs:/cvmfs -p 8888:8888 \
+    -p 8888:8888 \
     --user=root -e NB_UID="$(id -u)" -e NB_GID="$(id -g)" \
     neurodesktop:latest
+
+# Run with external CVMFS:
+# docker run --shm-size=1gb -it --cap-add SYS_ADMIN --security-opt apparmor:unconfined \
+#     --device=/dev/fuse --name neurodesktop -v ~/neurodesktop-storage:/neurodesktop-storage \
+#     -v /cvmfs:/cvmfs -p 8888:8888 \
+#     --user=root -e NB_UID="$(id -u)" -e NB_GID="$(id -g)" \
+#     neurodesktop:latest
