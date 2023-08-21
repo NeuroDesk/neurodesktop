02138dc63433ebd50b9802f4dff75c6cc755b6a4
aswinnarayanan <aswinn89@gmail.com>
2023-08-21 07:21:29 +0000

    Disable startup non-singleuser startup scripts

--- config/jupyter/before_notebook.sh
+++ config/jupyter/before_notebook.sh
@@ -1,103 +1,103 @@
-# This script runs in local Jupyterlab only (e.g. Docker, Neurodeskapp)
-# This script does NOT run on stock JupterHub/BinderHub instances (e.g. kubernetes)
-# For global startup script, see ./config/jupyter/jupterlab_startup.sh
+# # This script runs in local Jupyterlab only (e.g. Docker, Neurodeskapp)
+# # This script does NOT run on stock JupterHub/BinderHub instances (e.g. kubernetes)
+# # For global startup script, see ./config/jupyter/jupterlab_startup.sh
 
-# Overrides Dockerfile changes to NB_USER
-/usr/bin/printf '%s\n%s\n' 'password' 'password' | passwd ${NB_USER}
-usermod --shell /bin/bash ${NB_USER}
+# # Overrides Dockerfile changes to NB_USER
+# /usr/bin/printf '%s\n%s\n' 'password' 'password' | passwd ${NB_USER}
+# usermod --shell /bin/bash ${NB_USER}
 
-# # Generate SSH keys
-# if [ ! -f "/home/${NB_USER}/.ssh/guacamole_rsa" ]; then
-#     ssh-keygen -t rsa -f /home/${NB_USER}/.ssh/guacamole_rsa -b 4096 -m PEM -N '' <<< n
-# fi
-# if [ ! -f "/home/${NB_USER}/.ssh/id_rsa" ]; then
-#     ssh-keygen -t rsa -f /home/${NB_USER}/.ssh/id_rsa -b 4096 -m PEM -N '' <<< n
-# fi
-# if [ ! -f "/home/${NB_USER}/.ssh/ssh_host_rsa_key" ]; then
-#     ssh-keygen -t rsa -f /home/${NB_USER}/.ssh/ssh_host_rsa_key -N '' <<< n
-# fi
+# # # Generate SSH keys
+# # if [ ! -f "/home/${NB_USER}/.ssh/guacamole_rsa" ]; then
+# #     ssh-keygen -t rsa -f /home/${NB_USER}/.ssh/guacamole_rsa -b 4096 -m PEM -N '' <<< n
+# # fi
+# # if [ ! -f "/home/${NB_USER}/.ssh/id_rsa" ]; then
+# #     ssh-keygen -t rsa -f /home/${NB_USER}/.ssh/id_rsa -b 4096 -m PEM -N '' <<< n
+# # fi
+# # if [ ! -f "/home/${NB_USER}/.ssh/ssh_host_rsa_key" ]; then
+# #     ssh-keygen -t rsa -f /home/${NB_USER}/.ssh/ssh_host_rsa_key -N '' <<< n
+# # fi
 
-# cat /home/${NB_USER}/.ssh/guacamole_rsa.pub >> /home/${NB_USER}/.ssh/authorized_keys
-# cat /home/${NB_USER}/.ssh/id_rsa.pub >> /home/${NB_USER}/.ssh/authorized_keys
+# # cat /home/${NB_USER}/.ssh/guacamole_rsa.pub >> /home/${NB_USER}/.ssh/authorized_keys
+# # cat /home/${NB_USER}/.ssh/id_rsa.pub >> /home/${NB_USER}/.ssh/authorized_keys
 
-# # Set .ssh directory permissions
-# chmod -R 700 .ssh && chown -R ${NB_UID}:${NB_GID} .ssh
+# # # Set .ssh directory permissions
+# # chmod -R 700 .ssh && chown -R ${NB_UID}:${NB_GID} .ssh
 
-# # Insert guacamole private key into user-mapping for ssh/sftp support
-# sed -i "/private-key/ r /home/${NB_USER}/.ssh/guacamole_rsa" /etc/guacamole/user-mapping.xml
+# # # Insert guacamole private key into user-mapping for ssh/sftp support
+# # sed -i "/private-key/ r /home/${NB_USER}/.ssh/guacamole_rsa" /etc/guacamole/user-mapping.xml
 
-# # Start and stop SSH server to initialize host
-# sudo service ssh restart
-# sudo service ssh stop
+# # # Start and stop SSH server to initialize host
+# # sudo service ssh restart
+# # sudo service ssh stop
 
-# # Create a symlink in home if /data is mounted
-# if mountpoint -q /data; then
-#     ln -s /data /home/${NB_USER}/data
-# fi
+# # # Create a symlink in home if /data is mounted
+# # if mountpoint -q /data; then
+# #     ln -s /data /home/${NB_USER}/data
+# # fi
 
-if [ ! -d "/cvmfs/neurodesk.ardc.edu.au/neurodesk-modules/" ]; then
-    # the cvmfs directory is not yet mounted
-    if [ -z "$CVMFS_DISABLE" ]; then
-        # CVMFS is not disabled
+# if [ ! -d "/cvmfs/neurodesk.ardc.edu.au/neurodesk-modules/" ]; then
+#     # the cvmfs directory is not yet mounted
+#     if [ -z "$CVMFS_DISABLE" ]; then
+#         # CVMFS is not disabled
 
-        # try to list the directory in case it's autofs mounted outside
-        ls /cvmfs/neurodesk.ardc.edu.au/neurodesk-modules/ 2>/dev/null && echo "CVMFS is ready" || echo "CVMFS directory not there. Trying internal fuse mount next."
+#         # try to list the directory in case it's autofs mounted outside
+#         ls /cvmfs/neurodesk.ardc.edu.au/neurodesk-modules/ 2>/dev/null && echo "CVMFS is ready" || echo "CVMFS directory not there. Trying internal fuse mount next."
 
-        if [ ! -d "/cvmfs/neurodesk.ardc.edu.au/neurodesk-modules/" ]; then
-            # it is not available outside, so try mounting with fuse inside container
+#         if [ ! -d "/cvmfs/neurodesk.ardc.edu.au/neurodesk-modules/" ]; then
+#             # it is not available outside, so try mounting with fuse inside container
 
-            echo "\
-            ==================================================================
-            Mounting CVMFS"
-            if ( service autofs status > /dev/null ); then
-                 echo "autofs is running - not attempting to mount manually:"
-                 ls /cvmfs/neurodesk.ardc.edu.au/neurodesk-modules/ 2>/dev/null && echo "CVMFS is ready after autofs mount" || echo "AutoFS not working!"
-            else
-                echo "autofs is NOT running - attempting to mount manually:"
-                mkdir -p /cvmfs/neurodesk.ardc.edu.au
-                mount -t cvmfs neurodesk.ardc.edu.au /cvmfs/neurodesk.ardc.edu.au
+#             echo "\
+#             ==================================================================
+#             Mounting CVMFS"
+#             if ( service autofs status > /dev/null ); then
+#                  echo "autofs is running - not attempting to mount manually:"
+#                  ls /cvmfs/neurodesk.ardc.edu.au/neurodesk-modules/ 2>/dev/null && echo "CVMFS is ready after autofs mount" || echo "AutoFS not working!"
+#             else
+#                 echo "autofs is NOT running - attempting to mount manually:"
+#                 mkdir -p /cvmfs/neurodesk.ardc.edu.au
+#                 mount -t cvmfs neurodesk.ardc.edu.au /cvmfs/neurodesk.ardc.edu.au
 
-                ls /cvmfs/neurodesk.ardc.edu.au/neurodesk-modules/ 2>/dev/null && echo "CVMFS is ready after manual mount" || echo "Manual CVMFS mount not successful"
+#                 ls /cvmfs/neurodesk.ardc.edu.au/neurodesk-modules/ 2>/dev/null && echo "CVMFS is ready after manual mount" || echo "Manual CVMFS mount not successful"
 
-                echo "\
-                ==================================================================
-                Testing which CVMFS server is fastest"
-                cvmfs_talk -i neurodesk.ardc.edu.au host probe
-                cvmfs_talk -i neurodesk.ardc.edu.au host info
-            fi
-        fi
-    fi
-fi
-
-# source /opt/neurodesktop/environment_variables.sh
-
-# # clean up old session files (they prevent the start of the next session):
-# echo "starting cleanup before if"
-# if [ -f "/home/${NB_USER}/.ssh/id_rsa" ]
-# then
-#     echo "starting cleanup"
-#     rm /home/${NB_USER}/.ssh/id_rsa
-#     rm /home/${NB_USER}/.ssh/authorized_keys
-#     rm /home/${NB_USER}/.ssh/id_rsa.pub
-#     rm /home/${NB_USER}/.ssh/ssh_host_rsa_key
-#     rm /home/${NB_USER}/.ssh/ssh_host_rsa_key.pub 
-#     rm /home/${NB_USER}/.ssh/sshd.pid
-#     rm /home/${NB_USER}/.Xauthority
-#     rm -rf /home/${NB_USER}/.dbus/session-bus
-#     rm -rf /home/${NB_USER}/.vnc
-#     cp -r /tmp/${NB_USER}/.vnc /home/${NB_USER}/
+#                 echo "\
+#                 ==================================================================
+#                 Testing which CVMFS server is fastest"
+#                 cvmfs_talk -i neurodesk.ardc.edu.au host probe
+#                 cvmfs_talk -i neurodesk.ardc.edu.au host info
+#             fi
+#         fi
+#     fi
 # fi
 
-# # update example directory
-# if [ -d "/home/${NB_USER}/example-notebooks" ]
-# then
-#     cd /home/${NB_USER}/example-notebooks
-#     git pull
-# else
-#     git clone https://github.com/NeuroDesk/example-notebooks /home/${NB_USER}/example-notebooks
-# fi
+# # source /opt/neurodesktop/environment_variables.sh
 
-# cvmfs2 -o config=/cvmfs/neurodesk.ardc.edu.au.conf neurodesk.ardc.edu.au /cvmfs/neurodesk.ardc.edu.au
-# ssh-keygen -t rsa -f /home/${NB_USER}/.ssh/id_rsa -b 4096 -m PEM -N '' <<< n
-# ssh-keygen -t rsa -f /home/${NB_USER}/.ssh/ssh_host_rsa_key -N '' <<< n
-# cat /home/${NB_USER}/.ssh/id_rsa.pub >> /home/${NB_USER}/.ssh/authorized_keys
+# # # clean up old session files (they prevent the start of the next session):
+# # echo "starting cleanup before if"
+# # if [ -f "/home/${NB_USER}/.ssh/id_rsa" ]
+# # then
+# #     echo "starting cleanup"
+# #     rm /home/${NB_USER}/.ssh/id_rsa
+# #     rm /home/${NB_USER}/.ssh/authorized_keys
+# #     rm /home/${NB_USER}/.ssh/id_rsa.pub
+# #     rm /home/${NB_USER}/.ssh/ssh_host_rsa_key
+# #     rm /home/${NB_USER}/.ssh/ssh_host_rsa_key.pub 
+# #     rm /home/${NB_USER}/.ssh/sshd.pid
+# #     rm /home/${NB_USER}/.Xauthority
+# #     rm -rf /home/${NB_USER}/.dbus/session-bus
+# #     rm -rf /home/${NB_USER}/.vnc
+# #     cp -r /tmp/${NB_USER}/.vnc /home/${NB_USER}/
+# # fi
+
+# # # update example directory
+# # if [ -d "/home/${NB_USER}/example-notebooks" ]
+# # then
+# #     cd /home/${NB_USER}/example-notebooks
+# #     git pull
+# # else
+# #     git clone https://github.com/NeuroDesk/example-notebooks /home/${NB_USER}/example-notebooks
+# # fi
+
+# # cvmfs2 -o config=/cvmfs/neurodesk.ardc.edu.au.conf neurodesk.ardc.edu.au /cvmfs/neurodesk.ardc.edu.au
+# # ssh-keygen -t rsa -f /home/${NB_USER}/.ssh/id_rsa -b 4096 -m PEM -N '' <<< n
+# # ssh-keygen -t rsa -f /home/${NB_USER}/.ssh/ssh_host_rsa_key -N '' <<< n
+# # cat /home/${NB_USER}/.ssh/id_rsa.pub >> /home/${NB_USER}/.ssh/authorized_keys
--- config/jupyter/start_notebook.sh
+++ config/jupyter/start_notebook.sh
@@ -1,19 +1,19 @@
-# This script runs in local Jupyterlab only (e.g. Docker, Neurodeskapp)
-# This script does NOT run on stock JupterHub/BinderHub instances (e.g. kubernetes)
-# For global startup script, see ./config/jupyter/jupterlab_startup.sh
+# # This script runs in local Jupyterlab only (e.g. Docker, Neurodeskapp)
+# # This script does NOT run on stock JupterHub/BinderHub instances (e.g. kubernetes)
+# # For global startup script, see ./config/jupyter/jupterlab_startup.sh
 
-if [ -z "$GRANT_SUDO" ]; then
-export GRANT_SUDO='yes'
-fi
-if [ -z "$RESTARTABLE" ]; then
-export RESTARTABLE='yes'
-fi
+# if [ -z "$GRANT_SUDO" ]; then
+# export GRANT_SUDO='yes'
+# fi
+# if [ -z "$RESTARTABLE" ]; then
+# export RESTARTABLE='yes'
+# fi
 
-if [[ "$NB_UID" != "1000" || "$NB_GID" != "100" ]]; then
-    if [ -z "$CHOWN_HOME" ]; then
-    export CHOWN_HOME='yes'
-    fi
-    if [ -z "$CHOWN_HOME_OPTS" ]; then
-    export CHOWN_HOME_OPTS='-R'
-    fi
-fi
+# if [[ "$NB_UID" != "1000" || "$NB_GID" != "100" ]]; then
+#     if [ -z "$CHOWN_HOME" ]; then
+#     export CHOWN_HOME='yes'
+#     fi
+#     if [ -z "$CHOWN_HOME_OPTS" ]; then
+#     export CHOWN_HOME_OPTS='-R'
+#     fi
+# fi
