414230118f4bd3ffddf3bc55d8be1499e2e1de6b
Steffen Bollmann <stebo85@users.noreply.github.com>
2025-01-31 10:40:33 +1000

    add shebang

--- config/guacamole/setenv.sh
+++ config/guacamole/setenv.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 export JAVA_OPTS="-Xms512M -Xmx1024M"
 export CATALINA_OPTS="-Xms512M -Xmx1024M"
 # -XX:UseSVE=0 only exists on aarch64 so only add it if it exists. Check using `uname -m`
